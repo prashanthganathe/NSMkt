7267b896e7ca9d6c7359e5857582be978156f7d3
prashanthganathe <prashanthganathe@gmail.com>
2022-06-24 14:04:29 +0530

    home page final

---
+++ NSMkt/Views/Shared/_coicomparision.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NSMkt.Views.Shared
+{
+    public class _coicomparisionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

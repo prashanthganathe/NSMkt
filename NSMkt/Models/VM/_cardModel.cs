030c8588dd726e806d77941669711e5ef50165f3
prashanthganathe <prashanthganathe@gmail.com>
2022-06-26 14:59:02 +0530

    spartkline yet to work

---
+++ NSMkt/Models/VM/_cardModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NSMkt.Models.VM
+{
+    public class CardModel
+    {
+        public string title { get; set; }
+        public string type { get; set; }
+        public string css { get; set; }
+        public string value { get; set; }
+    }
+}
